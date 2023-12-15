45b64467053cd724b102a2491d06746a49cf01b4
josé carlos <josecampos2030.jdk@gmail.com>
2023-12-15 15:59:57 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 export default function App() {
   return (
-    <h1>OlÁ, Mundo</h1>
+    /*O REACT SÓ DEIXA COLOCAR MAISDE UM ELEMENTO SE ELES ESTIVEREM ENGLOBADOS POR ALGO.
+    COMO UMA DIV OU UM FRAGMENT(TAGS VAZIAS)*/
+    <>
+      <h1>OlÁ, Mundo</h1>
+      <p>Eae galera</p>
+      <h3>Back street boys são demais!</h3>
+    </>
   );
 }
