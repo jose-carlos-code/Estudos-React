5ebcbd029fbd19148722415a1b18008559bff8e8
josé carlos <carlinho2025@gmail.com>
2023-12-15 22:22:19 -0300

    Utilização de componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Main from './components/Main';
 import './App.css';
 export default function App() {
-  return (
-    /*O REACT SÓ DEIXA COLOCAR MAISDE UM ELEMENTO SE ELES ESTIVEREM ENGLOBADOS POR ALGO.
-    COMO UMA DIV OU UM FRAGMENT(TAGS VAZIAS)*/
-    <>
-      <h1>OlÁ, Mundo</h1>
-      <p>Eae galera</p>
-      <h3>Back street boys são demais!</h3>
-    </>
-  );
+  // return (
+  //   /*O REACT SÓ DEIXA COLOCAR MAISDE UM ELEMENTO SE ELES ESTIVEREM ENGLOBADOS POR ALGO.
+  //   COMO UMA DIV OU UM FRAGMENT(TAGS VAZIAS)*/
+  //   // <div>
+  //   //   <h1>OlÁ, Mundo</h1>
+  //   //   <p>Eae galera</p>
+  //   // </div>
+  // );
+  return <Main />
 }
